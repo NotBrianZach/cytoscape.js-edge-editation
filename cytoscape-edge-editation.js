a9ad616f8e536ea1383cfa95fcb6b79cdfbb072a
NotBrianZach <brianzacabel@utexas.edu>
2017-05-17 12:23:26 -0500

    wip

---
+++ cytoscape-edge-editation.js
@@ -0,0 +1,32 @@
+;(function(){ 'use strict';
+
+  // registers the extension on a cytoscape lib ref
+  var register = function( cytoscape ){
+
+    if( !cytoscape ){ return; } // can't register if cytoscape unspecified
+
+    cytoscape( 'core', 'edgeEditation', function(){
+      var cy = this;
+
+      // your extension impl...
+
+      return this; // chainability
+    } );
+
+  };
+
+  if( typeof module !== 'undefined' && module.exports ){ // expose as a commonjs module
+    module.exports = function( cytoscape ){
+      register( cytoscape );
+    };
+  } else if( typeof define !== 'undefined' && define.amd ){ // expose as an amd/requirejs module
+    define('cytoscape-edge-editation', function(){
+      return register;
+    });
+  }
+
+  if( typeof cytoscape !== 'undefined' ){ // expose to global cytoscape (i.e. window.cytoscape)
+    register( cytoscape );
+  }
+
+})();
